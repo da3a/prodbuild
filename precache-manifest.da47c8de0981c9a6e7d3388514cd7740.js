f2439b57904ff4b5bd877f886d33453331e0d23a
David Wallwin <dawa@lubrizol.com>
2019-12-23 09:21:12 +0000

    Updates

---
+++ precache-manifest.da47c8de0981c9a6e7d3388514cd7740.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e865af71646a0aba9be5e6d7c1d8b201",
+    "url": "/prodbuild/index.html"
+  },
+  {
+    "revision": "7f3d8d3ed4678ccb0264",
+    "url": "/prodbuild/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "9dbd79bee5279f723524",
+    "url": "/prodbuild/static/js/2.25561973.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/prodbuild/static/js/2.25561973.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7f3d8d3ed4678ccb0264",
+    "url": "/prodbuild/static/js/main.114528db.chunk.js"
+  },
+  {
+    "revision": "f5b4f89f318b7f4a35ed",
+    "url": "/prodbuild/static/js/runtime-main.14f2803a.js"
+  }
+]);
